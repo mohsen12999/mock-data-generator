78bf021aa29c2b0ebae36a75511d3de6496faf94
mohsen1299@gmail.com <mohsen1299@gmail.com>
2024-12-07 01:54:00 +0330

    add email option and save function

--- src/components/DataCard.vue
+++ src/components/DataCard.vue
@@ -4,6 +4,17 @@ import { ref } from "vue";
 defineProps<{ msg: string }>();
 
 // const count = ref(0)
+const downloadfile = () => {
+  console.log("downloadfile");
+
+  // const link = document.createElement("a");
+  // const content = document.querySelector("textarea").value;
+  // const file = new Blob([content], { type: 'text/plain' });
+  // link.href = URL.createObjectURL(file);
+  // link.download = "sample.txt";
+  // link.click();
+  // URL.revokeObjectURL(link.href);
+}
 </script>
 
 <template>
@@ -26,22 +37,27 @@ defineProps<{ msg: string }>();
         <option value="number">عدد</option>
         <option value="first_name">نام</option>
         <option value="last_name">نام خانوادگی</option>
+        <option value="email">ایمیل</option>
         <option value="city">شهر</option>
         <option value="country">کشور</option>
       </select>
-      <input type="text" name="" id="" />
+      <input type="number" class="empty_percentage" name="" id="" value="0" />
+      درصد خالی بودن
       <button type="button">حذف ستون</button>
     </div>
-    
+
   </div>
 
   <div class="card">
-    <button type="button">ساخت فایل</button>
+    <button type="button" @click="downloadfile()">ساخت فایل</button>
   </div>
 </template>
 
 <style scoped>
-.card > * {
+.card>* {
   margin-right: 10px;
+}
+.empty_percentage{
+  width: 3rem;
 }
 </style>
