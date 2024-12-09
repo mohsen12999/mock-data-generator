49eb93b5bcaf1d336aaf129e43ae60ca24aa9c0d
mohsen1299@gmail.com <mohsen1299@gmail.com>
2024-12-09 11:21:30 +0330

    add interface for columns

---
+++ src/functions/interfaces.ts
@@ -0,0 +1,9 @@
+export interface IColumnType {
+    column_name: string;
+    column_type: string;
+    empty_percentage: string;
+
+    min_number_value?: string;
+    max_number_value?: string;
+    decimal_number_value?: string;
+}
