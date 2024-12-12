207045229086790024d3aac5955320ac0eda2302
mohsen1299@gmail.com <mohsen1299@gmail.com>
2024-12-12 18:32:29 +0330

    add fake data generator in faker function

--- src/functions/faker.ts
+++ src/functions/faker.ts
@@ -1,3 +1,10 @@
+import { generateFakeAddress, generateFakeCity, generateFakeCountry } from "../provider/address";
+import { generatePersianAddress, generatePersianCity, GeneratePersianCountry } from "../provider/address-fa";
+import { generateFakeDomainName, generateFakeEmail } from "../provider/Internet";
+import { generateFakeFirstName, generateFakeFullname, generateFakeLastName } from "../provider/person";
+import { generateNationalCode, generatePersianFirstName, generatePersianFullName, generatePersianLastName } from "../provider/person-fa";
+import { randomNumber } from "./helper";
+
 export function generatedFakeField(
   field_type: string,
   empty_percentage: string,
@@ -6,9 +13,14 @@ export function generatedFakeField(
   max_number_value?: string,
   decimal_number_value?: string
 ): string | undefined {
-  const data_chance = Math.floor(Math.random() * 100);
-  if (data_chance < Number(empty_percentage)) {
-    return undefined;
+
+  if (empty_percentage !== "0" && empty_percentage !== "") {
+    if(empty_percentage === "100") return undefined;
+
+    const data_chance = Math.floor(Math.random() * 100);
+    if (data_chance < Number(empty_percentage)) {
+      return undefined;
+    }
   }
 
   switch (field_type) {
@@ -18,6 +30,46 @@ export function generatedFakeField(
         max_number_value,
         decimal_number_value
       );
+
+    case "first_name":
+      return generateFakeFirstName();
+    case "last_name":
+      return generateFakeLastName();
+    case "full_name":
+      return generateFakeFullname();
+    case "city":
+      return generateFakeCity();
+    case "country":
+      return generateFakeCountry();
+    case "email":
+      return generateFakeEmail();
+    case "website":
+      return generateFakeDomainName();
+    case "address":
+      return generateFakeAddress();
+
+    // TODO: add more fake data
+    // case "phone_number":
+    //   return "+989123456789";
+    // case "text":
+    //   return "Lorem ipsum dolor sit amet";
+    // case "persian_number":
+    //   return "۴۵۴۱۴۲۱۲۳۴۵۶۷۸۹";
+
+    case "persian_first_name":
+      return generatePersianFirstName();
+    case "persian_last_name":
+      return generatePersianLastName();
+    case "persian_full_name":
+      return generatePersianFullName();
+    case "persian_id":
+      return generateNationalCode();
+    case "persian_country":
+      return GeneratePersianCountry();
+    case "persian_city":
+      return generatePersianCity();
+    case "persian_address":
+      return generatePersianAddress();
 
     default:
       return "";
@@ -32,8 +84,8 @@ export function generatedFakeNumber(
   const min_number = Number(min_number_value);
   const max_number = Number(max_number_value);
   const decimal_number = Number(decimal_number_value);
-  const random_number =
-    Math.floor(Math.random() * (max_number - min_number + 1)) + min_number;
+  const random_number = randomNumber(min_number, max_number);
+
   if (decimal_number === 0 || !decimal_number) {
     return random_number.toString();
   }
