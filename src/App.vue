f32ad290f3e4ba7c562ef1e1997885b3201d2fbf
mohsen1299@gmail.com <mohsen1299@gmail.com>
2024-12-13 01:29:18 +0330

    add version to the app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <script setup lang="ts">
-  import DataCard from './components/DataCard.vue'
+import DataCard from "./components/DataCard.vue";
+import { version } from "../package.json"; // Import version from package.json
 </script>
 
 <template>
   <div>
-    <h1>Data for Generate</h1>
+    <h1 id="main_title">Data for Generate</h1>
+    <span>
+      <a href="http://github.com/mohsen1299/generate-mock-data">
+      version {{ version }}
+        <img
+          src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+          alt="GitHub"
+          id="github-logo"
+        />
+      </a>
+    </span>
   </div>
   <DataCard />
 </template>
 
-<style scoped></style>
+<style scoped>
+#main_title {
+  margin-bottom: auto;
+}
+#github-logo {
+  width: 1rem;
+}
+</style>
